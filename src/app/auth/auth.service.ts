508dd0195525d933a7e530ed73895e366ce26cbc
Pierluigi Fimiano <pierluigi.fimiano@external.fcagroup.com>
2020-07-18 17:07:49 +0200

    Add guard, interceptor and service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable, Subscription, timer} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly ROOT_URL = 'http://localhost:8080/';
+  private readonly TIMEOUT = 3600 * 1000;
+
+  // tslint:disable-next-line:variable-name
+  private readonly _loginEvent$ = new BehaviorSubject<any>(null);
+  readonly loginEvent$ = this._loginEvent$.asObservable();
+
+  private timerSub: Subscription = null;
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  login(username: string, password: string): Observable<any> {
+    const url = this.ROOT_URL + 'login';
+    const body = {
+      username,
+      password
+    };
+    return this.httpClient.post(url, body).pipe(
+      tap((r) => {
+        this.setupTimeout(this.TIMEOUT);
+        this._loginEvent$.next(r);
+      })
+    );
+  }
+
+  logout() {
+    this.stopTimer();
+    this._loginEvent$.next(null);
+  }
+
+  getToken(): string {
+    return this._loginEvent$.getValue();
+  }
+
+  private stopTimer() {
+    if (this.timerSub !== null) {
+      this.timerSub.unsubscribe();
+      this.timerSub = null;
+    }
+  }
+
+  private setupTimeout(timeoutInMillis: number) {
+    this.stopTimer();
+    this.timerSub = timer(timeoutInMillis).subscribe(() => {
+      this.logout();
+    });
+  }
+
+}
