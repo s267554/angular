40d5d314adedac9c2a6a31713926ded29bffb825
Pierluigi Fimiano <pierluigi.fimiano@external.fcagroup.com>
2020-07-18 17:09:14 +0200

    Improve auth guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,21 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private readonly authService: AuthService) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.getToken() !== null;
+
   }
 
 }
