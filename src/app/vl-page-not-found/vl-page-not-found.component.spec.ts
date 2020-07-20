2f2083c42ebb62a69a4caeac499b370d40952b18
Pierluigi Fimiano <pierluigi.fimiano@external.fcagroup.com>
2020-07-20 09:43:35 +0200

    Oops

--- src/app/vl-page-not-found/vl-page-not-found.component.spec.ts
+++ src/app/vl-page-not-found/vl-page-not-found.component.spec.ts
@@ -1,20 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {PageNotFoundComponent} from './page-not-found.component';
+import {VlPageNotFoundComponent} from './vl-page-not-found.component';
 
-describe('PageNotFoundComponent', () => {
-  let component: PageNotFoundComponent;
-  let fixture: ComponentFixture<PageNotFoundComponent>;
+describe('VlPageNotFoundComponent', () => {
+  let component: VlPageNotFoundComponent;
+  let fixture: ComponentFixture<VlPageNotFoundComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PageNotFoundComponent]
+      declarations: [VlPageNotFoundComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PageNotFoundComponent);
+    fixture = TestBed.createComponent(VlPageNotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
